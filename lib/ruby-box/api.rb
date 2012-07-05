884037de7f592a232667af477e263de8b1bfff14
lkang <lkang@sbcglobal.net>
2012-07-05 10:41:00 -0700

    add support for download, folder create, paths. Refactored and added more tests

--- lib/ruby-box/api.rb
+++ lib/ruby-box/api.rb
@@ -4,74 +4,209 @@ require 'json'
 require 'net/http/post/multipart'
 
 module RubyBox
-  class API
   
+  class FItem
+    attr_accessor :xport, :root_id
+    
+    def initialize( xport, root_id=0 )
+      @xport = xport
+      @root_id = root_id
+    end
+    
+  end
+  
+  class FFile < FItem
+    def delete
+      url = "https://api.box.com/2.0/files/#{@root_id}"
+      uri = URI.parse(url)
+      request = Net::HTTP::Delete.new( uri.request_uri )
+      raw = true
+      resp = @xport.do_http( uri, request, raw )
+    end
+    
+    def put_data( data, fname )
+      url = "https://upload.box.com/api/2.0/files/#{@root_id}data"
+      uri = URI.parse(url)
+      request = Net::HTTP::Post::Multipart.new(uri.path, {
+        "filename" => UploadIO.new(data, "application/pdf", fname),
+        "folder_id" => @root_id
+      })
+      @xport.do_http(uri, request)
+    end
+  end
+  
+  class FFolder < FItem
+    def file( name )
+      resp = list
+      return nil if resp['entries'].nil?
+      file_id = nil
+      resp["entries"].each do |item|
+        next if item["type"] != "file"
+        file_id = item["id"] if item["name"] == name
+      end
+      FFile.new( @xport, file_id )
+    end
+    
+    def folder( name )
+      resp = list
+      return nil if resp['entries'].nil?
+      file_id = nil
+      resp["entries"].each do |item|
+        next if item["type"] != "folder"
+        file_id = item["id"] if item["name"] == name
+      end
+      FFolder.new( @xport, file_id )
+    end
+    
+    
+    def delete
+      url = "https://api.box.com/2.0/folders/#{@root_id}"
+      uri = URI.parse(url)
+      request = Net::HTTP::Delete.new( uri.request_uri )
+      raw = true
+      resp = @xport.do_http( uri, request, raw )
+    end
+    
+    def list
+      url = "https://api.box.com/2.0/folders/#{@root_id}/items"
+      uri = URI.parse(url)
+      request = Net::HTTP::Get.new(uri.request_uri)
+      return @xport.do_http(uri, request)
+    end      
+        
+    def create( folder_name )
+      url = "https://api.box.com/2.0/folders#{@root_id}"
+      uri = URI.parse(url)
+      request = Net::HTTP::Post.new( uri.request_uri )
+      request.body = { "name" => folder_name }.to_json
+      resp = @xport.do_http( uri, request )
+      fid = resp["id"] if resp["name"] == folder_name
+      FItem.new( @xport, fid )
+    end
+    
+    def put_new_file( fname )
+      url = "https://upload.box.com/api/2.0/files/data"
+      uri = URI.parse(url)
+      File.open( fname ) do |file_stream|
+        request = Net::HTTP::Post::Multipart.new(uri.path, {
+          "filename" => UploadIO.new(file_stream, "application/text", file),
+          "folder_id" => @root_id
+        })
+        @xport.do_http(uri, request)
+      end
+    end
+    
+    def put_new_file_data( data, fname )
+      url = "https://upload.box.com/api/2.0/files/data"
+      uri = URI.parse(url)
+      request = Net::HTTP::Post::Multipart.new(uri.path, {
+        "filename" => UploadIO.new(data, "application/pdf", fname),
+        "folder_id" => @root_id
+      })
+      @xport.do_http(uri, request)
+    end    
+  end
+  
+  #user level - path inputs
+  class UserAPI
+    def initialize( xport )
+      @xport = xport
+      @folder = FFolder.new( @xport )
+    end
+    
+    def list( path )
+      fitem = folder( path )
+      return fitem.list
+    end
+    
+    def put_data( data, path, file )
+      fitem = create_path( path )
+      file_fitem = fitem.file( file )
+      if file_fitem.root_id.nil?
+        fitem.put_new_file_data( data, file )
+      else
+        file_fitem.put_data( data, file )
+      end
+    end
+    
+    def file( path )
+      folders = path.split('/')
+      fname = folders.pop
+      while !folders.empty? && folders.first.empty?
+        folders.shift
+      end
+      root_fitem = @folder
+      folders.each do |folder|
+        root_fitem = root_fitem.folder( folder )
+        return nil if root_fitem.nil?
+      end
+      root_fitem.file( fname )
+    end
+    
+    def download( path )
+      fitem = file( path )
+      return nil if fitem.nil?
+      # url = "https://api.box.com/2.0/files/#{fitem.root_id}/data" # bug: http://community.box.com/boxnet/topics/box_com_cant_down_file_used_api
+      url = "https://www.box.com/api/1.0/download/#{@xport.auth_token}/#{fitem.root_id}"  #api v1.0 - this does work
+      uri = URI.parse(url)
+      request = Net::HTTP::Get.new( uri.request_uri )
+      raw = true
+      resp = @xport.do_http( uri, request, raw )
+    end      
+    
+    def create_path( path )
+      folders = path.split('/')
+      while !folders.empty? && folders.first.empty?
+        folders.shift
+      end
+      root_fitem = @folder
+      folders.each do |folder|
+        next_folder = root_fitem.folder( folder ) # find it
+        unless next_folder.root_id 
+          next_folder = root_fitem.create( folder )      # else create it
+        end
+        root_fitem = next_folder
+      end
+      root_fitem
+    end
+          
+    def folder(path)
+      folders = path.split('/')
+      while !folders.empty? && folders.first.empty?
+        folders.shift
+      end
+      root_fitem = @folder
+      
+      folders.each do |folder|
+        root_fitem = root_fitem.folder( folder )
+        return nil if root_fitem.nil?
+      end
+      return root_fitem
+    end
+  end
+  
+  class Xport
+    attr_accessor :api_key, :auth_token
+    
     def initialize(api_key, auth_token)
       @api_key = api_key
       @auth_token = auth_token
     end
-    
-    def list(folder_id=0)
-      url = "https://api.box.com/2.0/folders/#{folder_id}/items"
-      uri = URI.parse(url)
-      request = Net::HTTP::Get.new(uri.request_uri)
-      return do_http(uri, request)
-    end
-    
-    def list_by_path(folder_path)
-      folder_id = get_folder_id_for_path(folder_path)
-      return list(folder_id)
-    end
-    
-    def put_file(folder_id, file)
-      url = "https://upload.box.com/api/2.0/files/data"
-      uri = URI.parse(url)
-      File.open(file) do |file_stream|
-        request = Net::HTTP::Post::Multipart.new(uri.path, {
-          "filename" => UploadIO.new(file_stream, "application/text", file),
-          "folder_id" => folder_id
-        })
-        do_http(uri, request)
-      end
-    end
-    
-    def put_file_in_folder_path(folder_path, file)
-      folder_id = get_folder_id_for_path(folder_path)
-      put_file(folder_id, file)
-    end 
-    
-    private
+    # low level communication routines
     
     def build_auth_header
       "BoxAuth api_key=#{@api_key}&auth_token=#{@auth_token}"
     end
     
-    def get_folder_id_for_path(folder_path)
-      current_folder_id = 0
-      split_folders = folder_path.split('/')
-      split_folders.each_with_index do |folder_name, index|
-        next if index == split_folders.length
-        list(current_folder_id)["entries"].each do |f|
-          if (f["type"] == "folder" and f["name"] == folder_name)
-            current_folder_id = f["id"]
-            break
-          end
-        end
-      end
-      return current_folder_id
-    end
-    
-    def do_http(uri, request)
+    def do_http(uri, request, raw=false)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
-      
       request.add_field('Authorization', build_auth_header)
-      
       response = http.request(request)
       if response.is_a? Net::HTTPNotFound
         raise RubyBox::ObjectNotFound
       end
-      return JSON.parse(response.body)
+      raw ? response.body : JSON.parse(response.body)
     end
   end
 end
