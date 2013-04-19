0ba04ff4ebe8fb04b7c8e16e43d04ff25e6b8498
bcoe <=>
2013-04-19 11:40:15 -0700

    made integration specs use a :skip setting, makes them less confusing.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,5 +1,6 @@
 #encoding: UTF-8
 
+require 'spec_helper'
 require 'helper/account'
 require 'ruby-box'
 require 'webmock/rspec'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+RSpec.configure do |c|
+  c.filter_run_excluding :skip => true
+end
