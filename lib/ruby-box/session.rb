a9e0c4dbfe7e0e25c45a52c444fa59a6e601d5d7
Russell Osborne <russosborn@gmail.com>
2013-11-15 11:56:10 -0500

    Adding As-User header support

--- lib/ruby-box/session.rb
+++ lib/ruby-box/session.rb
@@ -17,6 +17,7 @@ module RubyBox
         @oauth2_client = OAuth2::Client.new(opts[:client_id], opts[:client_secret], OAUTH2_URLS.dup)
         @access_token = OAuth2::AccessToken.new(@oauth2_client, opts[:access_token]) if opts[:access_token]
         @refresh_token = opts[:refresh_token]
+        @as_user = opts[:as_user] if opts[:as_user]
       else # Support legacy API for historical reasons.
         @api_key = opts[:api_key]
         @auth_token = opts[:auth_token]
@@ -64,6 +65,10 @@ module RubyBox
         request.add_field('Authorization', "Bearer #{@access_token.token}")
       else
         request.add_field('Authorization', build_auth_header)
+      end
+
+      if @as_user
+        request.add_field('As-User', "#{@as_user}")
       end
 
       response = http.request(request)
